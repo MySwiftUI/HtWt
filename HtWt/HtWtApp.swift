3de53b8341ff2bd90d4ce1f5d41ec3a415a13ba7
Jiseok97 <wltjr1426@naver.com>
2024-01-11 15:43:52 +0900

    [Set] 프로젝트 연결

---
+++ HtWt/HtWtApp.swift
@@ -0,0 +1,17 @@
+//
+//  HtWtApp.swift
+//  HtWt
+//
+//  Created by 이지석 on 2024/01/11.
+//
+
+import SwiftUI
+
+@main
+struct HtWtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
