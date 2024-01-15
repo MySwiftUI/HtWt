4e254d4326af2b569e7ded5a0e8aac0b90e8ae16
Jiseok97 <wltjr1426@naver.com>
2024-01-15 14:47:07 +0900

    [Add] HourlWeatherView(최상단) 추가

---
+++ HtWt/Sources/View/SubView/HourlyWeatherView.swift
@@ -0,0 +1,20 @@
+//
+//  HourlyWeatherView.swift
+//  HtWt
+//
+//  Created by 이지석 on 2024/01/13.
+//
+
+import SwiftUI
+
+struct HourlyWeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HourlyWeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        HourlyWeatherView()
+    }
+}
