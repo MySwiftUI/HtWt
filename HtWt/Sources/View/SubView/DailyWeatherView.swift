500a2d35978d1c34c1d23aa662960f4062bed081
Jiseok97 <wltjr1426@naver.com>
2024-01-16 16:16:59 +0900

    [Feat] 일간 날씨 UI 구현

--- HtWt/Sources/View/SubView/DailyWeatherView.swift
+++ HtWt/Sources/View/SubView/DailyWeatherView.swift
@@ -9,7 +9,59 @@ import SwiftUI
 
 struct DailyWeatherView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 52) {
+            Text("오늘")
+                .font(.system(size: 24))
+                .fontWeight(.medium)
+                .foregroundColor(.white)
+            
+            Image("clear")
+                .resizable()
+                .frame(width: 32, height: 32)
+            
+            HStack {
+                Text("-8º")
+                    .font(.system(size: 22))
+                    .fontWeight(.medium)
+                    .foregroundColor(.white)
+                    .opacity(0.4)
+                
+                ZStack {
+                    Capsule()
+                        .foregroundColor(.black)
+                        .opacity(0.1)
+                    
+                    GeometryReader { proxy in
+                        Capsule()
+                            .fill(.linearGradient(
+                                Gradient(colors: [.blue, .green, .yellow]),
+                                startPoint: .leading,
+                                endPoint: .trailing
+                            ))
+                            .frame(width: 80)   /// 10일 중 최저와 최대로 나누어 양 끝도 빌 수 있음
+                            .offset(x: 10)      /// offset X 잡아줘야 할 것으로 보임
+                    }
+                    
+                    Circle()
+                        .foregroundColor(.white)
+                        .overlay(Circle()
+                            .stroke(lineWidth: 3)
+                            .foregroundColor(.black).opacity(0.2)
+                        )
+                        .frame(width: 5, height: 5)
+                        .offset(x: -15)         /// 현재 온도 표시(offset x 위치 조정)
+                    
+                }
+                .frame(height: 5)
+                
+                Text("3º")
+                    .font(.system(size: 22))
+                    .fontWeight(.medium)
+                    .foregroundColor(.white)
+            }
+        }
+        .padding()
+        .background(.gray)
     }
 }
 
