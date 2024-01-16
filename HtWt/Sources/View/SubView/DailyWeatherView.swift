62608aa01a0f6e8ae45b0501fe657a1092411bf3
Jiseok97 <wltjr1426@naver.com>
2024-01-16 15:49:44 +0900

    [Add] DailyWeatherView 추가

---
+++ HtWt/Sources/View/SubView/DailyWeatherView.swift
@@ -0,0 +1,20 @@
+//
+//  DailyWeatherView.swift
+//  HtWt
+//
+//  Created by 이지석 on 2024/01/16.
+//
+
+import SwiftUI
+
+struct DailyWeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DailyWeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        DailyWeatherView()
+    }
+}
