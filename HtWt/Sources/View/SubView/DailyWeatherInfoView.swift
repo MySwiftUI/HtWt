c446cd2b46a26bfcd2742fd2dbb466457d418d7b
Jiseok97 <wltjr1426@naver.com>
2024-01-17 17:25:09 +0900

    [Feat] DailyWeatherView UI 수정

--- HtWt/Sources/View/SubView/DailyWeatherInfoView.swift
+++ HtWt/Sources/View/SubView/DailyWeatherInfoView.swift
@@ -9,59 +9,64 @@ import SwiftUI
 
 struct DailyWeatherInfoView: View {
     var body: some View {
-        HStack(spacing: 52) {
-            Text("오늘")
-                .font(.system(size: 24))
-                .fontWeight(.medium)
-                .foregroundColor(.white)
+        ZStack {
+            Rectangle()
+                .background(.clear)
+                .opacity(0)
             
-            Image("clear")
-                .resizable()
-                .frame(width: 32, height: 32)
-            
-            HStack {
-                Text("-8º")
-                    .font(.system(size: 22))
-                    .fontWeight(.medium)
+            HStack(spacing: 42) {
+                Text("오늘")
+                    .font(.system(size: 24))
+                    .fontWeight(.semibold)
                     .foregroundColor(.white)
-                    .opacity(0.4)
                 
-                ZStack {
-                    Capsule()
-                        .foregroundColor(.black)
-                        .opacity(0.1)
-                    
-                    GeometryReader { proxy in
-                        Capsule()
-                            .fill(.linearGradient(
-                                Gradient(colors: [.blue, .green, .yellow]),
-                                startPoint: .leading,
-                                endPoint: .trailing
-                            ))
-                            .frame(width: 80)   /// 10일 중 최저와 최대로 나누어 양 끝도 빌 수 있음
-                            .offset(x: 10)      /// offset X 잡아줘야 할 것으로 보임
-                    }
-                    
-                    Circle()
+                Image("clear")
+                    .resizable()
+                    .frame(width: 32, height: 32)
+                
+                HStack(spacing: 8) {
+                    Text("-8º")
+                        .font(.system(size: 22))
+                        .fontWeight(.semibold)
                         .foregroundColor(.white)
-                        .overlay(Circle()
-                            .stroke(lineWidth: 3)
-                            .foregroundColor(.black).opacity(0.2)
-                        )
-                        .frame(width: 5, height: 5)
-                        .offset(x: -15)         /// 현재 온도 표시(offset x 위치 조정)
+                        .opacity(0.4)
                     
+                    ZStack {
+                        Capsule()
+                            .foregroundColor(.black)
+                            .opacity(0.1)
+                        
+                        GeometryReader { proxy in
+                            Capsule()
+                                .fill(.linearGradient(
+                                    Gradient(colors: [.blue, .green, .yellow]),
+                                    startPoint: .leading,
+                                    endPoint: .trailing
+                                ))
+                                .frame(width: 80)   /// 10일 중 최저와 최대로 나누어 양 끝도 빌 수 있음
+                                .offset(x: 10)      /// offset X 잡아줘야 할 것으로 보임
+                        }
+                        
+                        Circle()
+                            .foregroundColor(.white)
+                            .overlay(Circle()
+                                .stroke(lineWidth: 3)
+                                .foregroundColor(.black).opacity(0.2)
+                            )
+                            .frame(width: 5, height: 5)
+                            .offset(x: -15)         /// 현재 온도 표시(offset x 위치 조정)
+                        
+                    }
+                    .frame(height: 5)
+                    
+                    Text("3º")
+                        .font(.system(size: 22))
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
                 }
-                .frame(height: 5)
-                
-                Text("3º")
-                    .font(.system(size: 22))
-                    .fontWeight(.medium)
-                    .foregroundColor(.white)
             }
+            .padding()
         }
-        .padding()
-        .background(.gray)
     }
 }
 
