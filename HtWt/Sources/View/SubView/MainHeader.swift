390fff61c436c9cba4a2698fa0de72330b47a84c
Jiseok97 <wltjr1426@naver.com>
2024-01-17 17:25:38 +0900

    [Feat] MainHeaderView Text 속성 수정

--- HtWt/Sources/View/SubView/MainHeader.swift
+++ HtWt/Sources/View/SubView/MainHeader.swift
@@ -17,28 +17,30 @@ struct MainHeader: View {
     var body: some View {
         VStack(alignment: .center) {
             Text(locationName)
-                .font(.system(size: 32))
+                .font(.system(size: 36))
                 .fontWeight(.regular)
             
             Text(currentTemp)
-                .font(.system(size: 84))
-                .fontWeight(.light)
+                .font(.system(size: 108))
+                .fontWeight(.thin)
             
             Text(weatherInfo)
-                .font(.system(size: 22))
+                .font(.system(size: 28))
                 .fontWeight(.light)
             
             HStack(spacing: 10) {
                 Text(maxTemp)
-                    .font(.system(size: 20))
+                    .font(.system(size: 24))
                     .fontWeight(.medium)
                 
                 Text(minTemp)
-                    .font(.system(size: 20))
+                    .font(.system(size: 24))
                     .fontWeight(.medium)
             }
             .padding(-10)
         }
+        .foregroundColor(.white)
+        .shadow(color: .secondary, radius: 8, x: 2, y: 2)
         .frame(minWidth: 0, maxWidth: .infinity)
         .frame(height: 230)
         .background(Color.clear)
@@ -51,8 +53,8 @@ struct MainHeader_Previews: PreviewProvider {
             locationName: "서울시",
             currentTemp: "5º",
             weatherInfo: "대체로 흐림",
-            maxTemp: "5º",
-            minTemp: "-5º"
+            maxTemp: "최고:5º",
+            minTemp: "최저:-5º"
         )
     }
 }
