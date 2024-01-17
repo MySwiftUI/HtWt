6a2917da5fdf86e4525a0d202752569e399332bf
Jiseok97 <wltjr1426@naver.com>
2024-01-17 17:26:06 +0900

    [Feat] DailyWeatherView UI 구현

--- HtWt/Sources/View/SubView/DailyWeatherView.swift
+++ HtWt/Sources/View/SubView/DailyWeatherView.swift
@@ -9,7 +9,40 @@ import SwiftUI
 
 struct DailyWeatherView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Rectangle()
+                .background(.gray)
+                .opacity(0.3)
+            
+            LazyVStack {
+                HStack {
+                    Image(systemName: "calendar")
+                        .resizable()
+                        .frame(width: 16, height: 16)
+                        .padding(.leading, 12)
+                    
+                    Text("10일간의 일기예보")
+                        .font(.system(size: 16))
+                        .fontWeight(.medium)
+                    
+                    Spacer()
+                }
+                .padding(.top, 16)
+                .foregroundColor(.white)
+                .opacity(0.6)
+                
+                ForEach(0..<10) { _ in
+                    Divider()
+                        .background(.white)
+                        .padding(.horizontal, 12)
+                    
+                    DailyWeatherInfoView()
+                        .padding(.vertical, -12)
+                }
+            }
+            .padding(.bottom, 8)
+        }
+        .cornerRadius(12)
     }
 }
 
