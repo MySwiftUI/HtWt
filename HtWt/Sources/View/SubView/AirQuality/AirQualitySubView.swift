e0fed64f714c0631f3fad279fc031eee27252872
Jiseok97 <wltjr1426@naver.com>
2024-01-18 16:43:16 +0900

    [Add] AirQualityView & SubView 추가

---
+++ HtWt/Sources/View/SubView/AirQuality/AirQualitySubView.swift
@@ -0,0 +1,20 @@
+//
+//  AirQualitySubView.swift
+//  HtWt
+//
+//  Created by 이지석 on 2024/01/18.
+//
+
+import SwiftUI
+
+struct AirQualitySubView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AirQualitySubView_Previews: PreviewProvider {
+    static var previews: some View {
+        AirQualitySubView()
+    }
+}
---
+++ HtWt/Sources/View/SubView/AirQuality/AirQualityView.swift
@@ -0,0 +1,20 @@
+//
+//  AirQualityView.swift
+//  HtWt
+//
+//  Created by 이지석 on 2024/01/18.
+//
+
+import SwiftUI
+
+struct AirQualityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AirQualityView_Previews: PreviewProvider {
+    static var previews: some View {
+        AirQualityView()
+    }
+}
