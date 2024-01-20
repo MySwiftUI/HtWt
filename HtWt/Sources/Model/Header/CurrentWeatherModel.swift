f330bdf88101b964eb2bd6599c450a75bd70509f
Jiseok97 <wltjr1426@naver.com>
2024-01-20 23:36:05 +0900

    [Add] MainHeaderViewModel, CurrentWeatherModel 추가

---
+++ HtWt/Sources/Model/Header/CurrentWeatherModel.swift
@@ -0,0 +1,8 @@
+//
+//  CurrentWeatherModel.swift
+//  HtWt
+//
+//  Created by 이지석 on 2024/01/20.
+//
+
+import Foundation
---
+++ HtWt/Sources/ViewModel/Header/MainHeaderViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MainHeaderViewModel.swift
+//  HtWt
+//
+//  Created by 이지석 on 2024/01/20.
+//
+
+import Foundation
