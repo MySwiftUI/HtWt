1351098cf811d0dd76291a57386a859f33903a31
Jiseok97 <wltjr1426@naver.com>
2024-01-24 18:40:03 +0900

    [Feat] HourlyWeatherViewModel API 통신 기능 구현

--- HtWt/Sources/ViewModel/HourlyWeather/HourlWeatherViewModel.swift
+++ HtWt/Sources/ViewModel/HourlyWeather/HourlWeatherViewModel.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+import Alamofire
+import Combine
+import CoreLocation
+
+final class HourlWeatherViewModel: ObservableObject {
+    @Published var wData: HourlyWeatherModel?
+    
+    private var location: CLLocationCoordinate2D
+    private lazy var cancellable = Set<AnyCancellable>()
+    
+    init(
+        location: CLLocationCoordinate2D
+    ) {
+        self.location = location
+        requestHourlyWeather()
+    }
+    
+    private func requestHourlyWeather() {
+        print("DEBUG: requestHourlyWeather() --- ")
+        let param: [String:Any] = [
+            "units" : "metric",
+            "lang" : "KR",
+            "lat" : location.latitude,
+            "lon" : location.longitude,
+            "appid" : Bundle.main.apiKey
+        ]
+        
+        AF.request(
+            Constants.FIVEDAYS_THREEHOURS_URL,
+            method: .get,
+            parameters: param,
+            encoding: URLEncoding.queryString
+        )
+        .publishDecodable(type: HourlyWeatherModel.self)
+        .compactMap { $0.value }
+        .sink(receiveCompletion: { completion in
+            print("DEBUG: requestHourlyWeather() 실행이 완료되었습니다.")
+        }, receiveValue: { response in
+            print("DEBUG: response is \(response)")
+        })
+        .store(in: &cancellable)
+    }
+}
