7310d2973731b1497893fe7112c6dd9516534bb8
Jiseok97 <wltjr1426@naver.com>
2024-01-30 23:06:57 +0900

    [Feat] HourlyWeatherModel 핸들링 및 UI를 위한 시간 체크 기능, 커스텀 기능 추가

--- HtWt/Sources/ViewModel/HourlyWeather/HourlWeatherViewModel.swift
+++ HtWt/Sources/ViewModel/HourlyWeather/HourlWeatherViewModel.swift
@@ -11,7 +11,7 @@ import Combine
 import CoreLocation
 
 final class HourlyWeatherViewModel: ObservableObject {
-    @Published var hourlyWeatherData: [HourlyWeatherData] = []
+    @Published var hourlyWeatherViewItem: [HourlyWeatherItem] = []
     
     private var cancellable = Set<AnyCancellable>()
     
@@ -49,8 +49,75 @@ final class HourlyWeatherViewModel: ObservableObject {
                 print("DEBUG: requestHourlyWeather() 서버 통신 에러입니다.\n\(error.localizedDescription)")
             }
         }, receiveValue: { response in
-            self.hourlyWeatherData = Array(response.list.prefix(24))
+            self.hourlyWeatherDataHandler(data: response.list)
         })
         .store(in: &cancellable)
+    }
+}
+
+private extension HourlyWeatherViewModel {
+    /// 현재시간보다 이전인지 체크하기 위한 기능
+    func checkCurrentTime(
+        from sendDate: String
+    ) -> Bool {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        dateFormatter.timeZone = TimeZone(identifier: "KST")
+        return dateFormatter.string(from: Date()) < sendDate
+    }
+    
+    /// 내일모레를 초과하지 않기 위해 체크하는 기능
+    func checkTwoDaysLater(
+        from sendDate: String
+    ) -> Bool {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        dateFormatter.timeZone = TimeZone(identifier: "KST")
+        return sendDate < dateFormatter.string(from: Calendar.current.date(bySetting: .day, value: 2, of: Date()) ?? Date())
+    }
+    
+    /// 전체 시간을 시간으로만 변환 시켜주는 기능
+    func changeFullDateToHourString(
+        from fullDate: String
+    ) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        
+        if let date = dateFormatter.date(from: fullDate) {
+            let timeFormatter = DateFormatter()
+            timeFormatter.dateFormat = Calendar.current.component(.hour, from: date) < 12
+            ? "오전 h시"
+            : "오후 h시"
+            
+            return timeFormatter.string(from: date)
+        }
+        
+        else { return "" }
+    }
+    
+    /// HourlyWeatherView에서 필요한 데이터만 핸들링 하는 기능
+    func hourlyWeatherDataHandler(
+        data: [HourlyWeatherData]
+    ) {
+        var appendData = HourlyWeatherItem(
+            id: UUID(),
+            timeText: "",
+            weatherImageName: "clear",
+            tempText: ""
+        )
+        
+        for i in 0..<data.count {
+            let time = data[i].dtTxt
+            if !(checkCurrentTime(from: time) && checkTwoDaysLater(from: time)) { continue }
+            
+            let uuid: String = String(describing: data[i].weather.first?.id)
+            appendData.id = UUID(uuidString: uuid) ?? UUID()
+            appendData.timeText = changeFullDateToHourString(from: time)
+            appendData.weatherImageName = data[i].weather.first?.icon ?? "clear"
+            appendData.tempText = "\(Int(data[i].main.temp))º"
+            hourlyWeatherViewItem.append(appendData)
+        }
     }
 }
